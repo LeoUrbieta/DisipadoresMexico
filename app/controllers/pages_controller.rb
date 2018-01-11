58af96895f98a77b14102f1fb72d3edc1a8ba4b6
Leo Moreno <achicopalado@gmail.com>
2018-01-11 00:04:03 +0000

    Arreglar errores

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def ingresos
-    @ventas = Venta.paginate(page: params[:page], per_page: 20)
+    @ventas = Venta.all.order("fecha")
   end
 
   def buscar
