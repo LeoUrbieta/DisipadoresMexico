b788bc1e579aee7959bf6c4dc9603798318deb9d
Leo Moreno <achicopalado@gmail.com>
2018-02-09 15:50:25 +0000

    Añadir mas versatilidad a la busqueda por fechas en ingresos

--- app/assets/javascripts/pages.coffee
+++ app/assets/javascripts/pages.coffee
@@ -2,9 +2,24 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+
 $(document).on 'turbolinks:load', ->
+  
   $('.datepicker').datepicker({
     format: 'dd/mm/yyyy',
     autoclose: true,
   })
+  
+  $('.campos_fecha_final').hide()
+  
+  $('#inicio_datepicker').datepicker().on 'changeDate', ->
+    $('.campos_fecha_final').show()
+    fecha_inicial = $('#inicio_datepicker').datepicker('getDate')
+    $('#final_datepicker').datepicker('update', fecha_inicial)
+    $('#final_datepicker').datepicker('setStartDate', fecha_inicial)
+    
+    
+  
+  
+ 
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,11 @@ class PagesController < ApplicationController
     fecha_inicial = params['fecha_inicial'].split('/')
     fecha_final = params['fecha_final'].split('/')
     
-    fecha_inicio = Date::civil(fecha_inicial[2].to_i, fecha_inicial[1].to_i,fecha_inicial[0].to_i)
-    fecha_final = Date::civil(fecha_final[2].to_i, fecha_final[1].to_i, fecha_final[0].to_i)
+    @fecha_inicio = Date::civil(fecha_inicial[2].to_i, fecha_inicial[1].to_i,fecha_inicial[0].to_i)
+    @fecha_final = Date::civil(fecha_final[2].to_i, fecha_final[1].to_i, fecha_final[0].to_i)
     
     @claveSAT = Producto.select("nombre_producto, clave_sat")
-    @ventas = Venta.busca_productos(fecha_inicio,fecha_final,params[:facturado]).order("fecha")
+    @ventas = Venta.busca_productos(@fecha_inicio,@fecha_final,params[:facturado]).order("fecha")
     @suma = Venta.suma_productos(@ventas)
     
     render 'ingresos'
