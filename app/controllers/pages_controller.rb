5390bf694c5d7c2c7e99b24cb04c3a37dfd2e321
Leo Moreno <achicopalado@gmail.com>
2018-02-13 19:33:51 +0000

    Añadir precios unitarios y cambiar layout de ingresos para mayor claridad

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,17 +6,25 @@ class PagesController < ApplicationController
 
   def buscar
     
-    fecha_inicial = params['fecha_inicial'].split('/')
-    fecha_final = params['fecha_final'].split('/')
+    if( params['fecha_inicial'].empty?)
     
-    @fecha_inicio = Date::civil(fecha_inicial[2].to_i, fecha_inicial[1].to_i,fecha_inicial[0].to_i)
-    @fecha_final = Date::civil(fecha_final[2].to_i, fecha_final[1].to_i, fecha_final[0].to_i)
+      render 'ingresos'
+      
+    else
+      
+      fecha_inicial = params['fecha_inicial'].split('/')
+      fecha_final = params['fecha_final'].split('/')
+      
+      @fecha_inicio = Date::civil(fecha_inicial[2].to_i, fecha_inicial[1].to_i,fecha_inicial[0].to_i)
+      @fecha_final = Date::civil(fecha_final[2].to_i, fecha_final[1].to_i, fecha_final[0].to_i)
+      
+      @claveSAT = Producto.select("nombre_producto, clave_sat")
+      @ventas = Venta.busca_productos(@fecha_inicio,@fecha_final,params[:facturado]).order("fecha")
+      @suma = Venta.suma_productos(@ventas)
+ 
+      render 'ingresos'
     
-    @claveSAT = Producto.select("nombre_producto, clave_sat")
-    @ventas = Venta.busca_productos(@fecha_inicio,@fecha_final,params[:facturado]).order("fecha")
-    @suma = Venta.suma_productos(@ventas)
-    
-    render 'ingresos'
+    end
     
   end
   
--- app/models/venta.rb
+++ app/models/venta.rb
@@ -32,10 +32,13 @@ class Venta < ApplicationRecord
   
   def self.suma_productos(ventas)
     suma_productos = Hash.new
-    nombre_productos = ["longitud_75mm","precio_75mm", "longitud_87mm",
-    "precio_87mm", "longitud_136mm", "precio_136mm","cantidad_peltier",
-    "precio_peltier", "cantidad_pasta_termica" , "precio_pasta_termica", 
-    "total_productos","envio_explicito", "envio_agregado_a_precio_productos"]
+    nombre_productos = ["longitud_75mm","precio_75mm", "descuento_75mm",    
+                        "longitud_87mm", "precio_87mm","descuento_87mm", 
+                        "longitud_136mm", "precio_136mm","descuento_136mm",
+                        "cantidad_peltier","precio_peltier","descuento_peltier", 
+                        "cantidad_pasta_termica","precio_pasta_termica","descuento_pasta_termica",
+                        "total_productos","envio_explicito", "envio_agregado_a_precio_productos",
+                        "dinero_anadido","total_pagado_por_cliente"]
     
     nombre_productos.each do |suma_producto|
       suma_producto_actual = BigDecimal.new('0.0')
@@ -44,7 +47,35 @@ class Venta < ApplicationRecord
       end
       suma_productos[suma_producto] = suma_producto_actual
     end
+    
+    IncluyePrecioUnitario(suma_productos)
     return suma_productos
+  end
+  
+  def self.IncluyePrecioUnitario(suma)
+    nombre_producto = ["75mm","87mm","136mm","peltier","pasta_termica"]
+    
+    nombre_producto.each do |producto|
+      if(suma["longitud_"<< producto] != nil && suma["longitud_"<< producto] != 0)
+       suma["precio_unitario_" << producto] = (((suma["precio_"<< producto] - suma["descuento_"<< producto]) / (suma["longitud_"<< producto])) / 1.16).round(2)
+      elsif (suma["cantidad_"<< producto] != nil  && suma["cantidad_"<< producto] != 0)
+        suma["precio_unitario_" << producto] = (((suma["precio_"<< producto] - suma["descuento_"<< producto]) / (suma["cantidad_"<< producto])) / 1.16).round(2)
+      else
+        suma["precio_unitario_" << producto] = "No hubo ventas de este producto"
+      end
+      
+    end
+    
+    AnadirEnviosYDineroAnadidoUnitarios(suma)
+    
+  end
+  
+  def self.AnadirEnviosYDineroAnadidoUnitarios(suma_conceptos)
+    
+    suma_conceptos["precio_unitario_envio_explicito"] = (suma_conceptos["envio_explicito"] / 1.16).round(2)
+    suma_conceptos["precio_unitario_servicios_extra"] = ((suma_conceptos["envio_agregado_a_precio_productos"] +
+                                                          suma_conceptos["dinero_anadido"]) / 1.16).round(2)
+    
   end
   
   def self.total_disponible(ventas,egresos)
