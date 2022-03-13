86d306abc8221ce8d7c028970dc0e674b3ad032f
Leo Moreno <leomoreno@hey.com>
2022-03-13 04:19:12 -0600

    Actulizar a Ruby 2.7.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190123175456) do
+ActiveRecord::Schema.define(version: 2019_01_23_175456) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "nombre"
